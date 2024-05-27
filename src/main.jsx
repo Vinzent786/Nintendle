a30da330b0a02c98118dda02e416eb9020f82702
Vinzent786 <vincentcook6864@gmail.com>
2024-05-27 15:57:56 -0400

    Updated styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
-import './styles/master.css'
+import './styles/master.css?v=1.1'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
