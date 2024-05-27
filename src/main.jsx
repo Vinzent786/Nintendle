943724d03f100b2350934de833b06cd9af1a30e1
Vinzent786 <vincentcook6864@gmail.com>
2024-05-27 18:12:49 -0400

    Edited height units

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
-import './styles/master.css?v=1.1'
+import './styles/master.css?v=1.2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
